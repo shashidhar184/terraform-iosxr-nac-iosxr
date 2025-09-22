b65998e301891ce9809d64671710ee6702fd67c4
shashidhar184 <shashidhar184@gmail.com>
2025-09-22 18:44:54 +0530

    Resolve merge conflict in iosxr_l2vpn_bridge_group.tf

--- iosxr_l2vpn_bridge_group.tf
+++ iosxr_l2vpn_bridge_group.tf
@@ -1,19 +1,19 @@
 locals {
   device_l2vpn_bridge_groups = flatten([
     for device in local.devices : [
-      for bridge_group in try(local.device_config[device.name].l2vpn_bridge_group, []) : {
+      for bg in try(local.device_config[device.name].l2vpn_bridge_group, local.defaults.iosxr.configuration.l2vpn_bridge_group, []) : {
         device_name = device.name
-        group_name  = try(bridge_group.group_name, local.defaults.iosxr.configuration.l2vpn_bridge_group_group_name, null)
-        delete_mode = try(bridge_group.delete_mode, local.defaults.iosxr.configuration.l2vpn_bridge_group_delete_mode, null)
-        key         = try("${device.name}-${bridge_group.group_name}", null)
+        group_name  = try(bg.group_name, local.defaults.iosxr.configuration.l2vpn_bridge_group_name, null)
+        delete_mode = try(bg.delete_mode, local.defaults.iosxr.configuration.l2vpn_bridge_group_delete_mode, null)
+        key         = try("${device.name}-${bg.group_name}", null)
       }
-      if try(bridge_group.group_name, local.defaults.iosxr.configuration.l2vpn_bridge_group_group_name, null) != null
+      if try(bg.group_name, local.defaults.iosxr.configuration.l2vpn_bridge_group_name, null) != null
     ]
   ])
 }
 
 resource "iosxr_l2vpn_bridge_group" "l2vpn_bridge_group" {
-  for_each = { for bridge_group in local.device_l2vpn_bridge_groups : bridge_group.key => bridge_group }
+  for_each = { for bg in local.device_l2vpn_bridge_groups : bg.key => bg }
 
   device      = each.value.device_name
   group_name  = each.value.group_name
