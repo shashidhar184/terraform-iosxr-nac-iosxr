24feb2148b67b6fa08863eaa78ec901c9cec1647
shashidhar184 <shashidhar184@gmail.com>
2025-09-26 20:56:54 +0530

    Updated

--- iosxr_rd_set.tf
+++ iosxr_rd_set.tf
@@ -2,10 +2,9 @@ locals {
   device_rd_sets = flatten([
     for device in local.devices : [
       for rd_set in try(local.device_config[device.name].rd_set, local.defaults.iosxr.configuration.rd_set, []) : {
-        device_name = device.name
-        set_name    = try(rd_set.set_name, local.defaults.iosxr.configuration.rd_set_name, null)
-        rpl         = try(rd_set.rpl, local.defaults.iosxr.configuration.rd_set_rpl, null)
-        key         = "${device.name}-rd_set-${rd_set.set_name}"
+        set_name = try(rd_set.set_name, local.defaults.iosxr.configuration.rd_set_name, null)
+        rpl      = try(rd_set.rpl, local.defaults.iosxr.configuration.rd_set_rpl, null)
+        key      = "${device.name}-rd_set-${rd_set.set_name}"
       }
       if try(rd_set.set_name, local.defaults.iosxr.configuration.rd_set_name, null) != null &&
       try(rd_set.rpl, local.defaults.iosxr.configuration.rd_set_rpl, null) != null
@@ -17,7 +16,6 @@ locals {
 resource "iosxr_rd_set" "rd_set" {
   for_each = { for rd_set in local.device_rd_sets : rd_set.key => rd_set }
 
-  device   = each.value.device_name
   set_name = each.value.set_name
   rpl      = each.value.rpl
 }
