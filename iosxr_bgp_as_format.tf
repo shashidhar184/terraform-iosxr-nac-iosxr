378e376fd2f2e657f5d400420f7859f02651cf5d
rocrowe <rocrowe@cisco.com>
2026-01-29 12:30:44 -0500

    fix

--- iosxr_bgp_as_format.tf
+++ iosxr_bgp_as_format.tf
@@ -1,5 +1,5 @@
 resource "iosxr_bgp_as_format" "bgp_as_format" {
-  for_each  = { for device in local.devices : device.name => device if try(local.device_config[device.name].bgp_as_format, null) != null || try(local.defaults.iosxr.devices.configuration.bgp_as_format, null) != null }
+  for_each  = { for device in local.devices : device.name => device if try(local.device_config[device.name].as_format, null) != null || try(local.defaults.iosxr.devices.configuration.as_format, null) != null }
   device    = each.value.name
-  as_format = try(local.device_config[each.value.name].bgp_as_format.as_format, local.defaults.iosxr.devices.configuration.bgp_as_format.as_format, null)
+  as_format = try(local.device_config[each.value.name].as_format, local.defaults.iosxr.devices.configuration.as_format, null)
 }
