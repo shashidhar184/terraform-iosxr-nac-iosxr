b06253073cba9830283626f190cf0cf9a86c0053
rocrowe <rocrowe@cisco.com>
2026-03-03 19:56:12 -0500

    fixes

--- iosxr_vty_pool.tf
+++ iosxr_vty_pool.tf
@@ -1,16 +1,18 @@
 resource "iosxr_vty_pool" "vty_pool" {
-  for_each              = { for device in local.devices : device.name => device if try(local.device_config[device.name].vty_pool, null) != null || try(local.defaults.iosxr.devices.configuration.vty_pool, null) != null }
+  for_each              = { for device in local.devices : device.name => device if try(local.device_config[device.name].vty_pools, null) != null || try(local.defaults.iosxr.devices.configuration.vty_pools, null) != null }
   device                = each.value.name
-  default_first_vty     = try(local.device_config[each.value.name].vty_pool.default_first_vty, local.defaults.iosxr.devices.configuration.vty_pool.default_first_vty, null)
-  default_last_vty      = try(local.device_config[each.value.name].vty_pool.default_last_vty, local.defaults.iosxr.devices.configuration.vty_pool.default_last_vty, null)
-  default_line_template = try(local.device_config[each.value.name].vty_pool.default_line_template, local.defaults.iosxr.devices.configuration.vty_pool.default_line_template, null)
-  eem_first_vty         = try(local.device_config[each.value.name].vty_pool.eem_first_vty, local.defaults.iosxr.devices.configuration.vty_pool.eem_first_vty, null)
-  eem_last_vty          = try(local.device_config[each.value.name].vty_pool.eem_last_vty, local.defaults.iosxr.devices.configuration.vty_pool.eem_last_vty, null)
-  eem_line_template     = try(local.device_config[each.value.name].vty_pool.eem_line_template, local.defaults.iosxr.devices.configuration.vty_pool.eem_line_template, null)
-  pools = try(length(local.device_config[each.value.name].vty_pool.pools) == 0, true) ? null : [for pool in local.device_config[each.value.name].vty_pool.pools : {
-    pool_name     = pool.pool_name
-    first_vty     = try(pool.first_vty, null)
-    last_vty      = try(pool.last_vty, null)
-    line_template = try(pool.line_template, null)
-  }]
+  default_first_vty     = try(local.device_config[each.value.name].vty_pools.default.from_number, local.defaults.iosxr.devices.configuration.vty_pools.default.from_number, null)
+  default_last_vty      = try(local.device_config[each.value.name].vty_pools.default.to_number, local.defaults.iosxr.devices.configuration.vty_pools.default.to_number, null)
+  default_line_template = try(local.device_config[each.value.name].vty_pools.default.line_template, local.defaults.iosxr.devices.configuration.vty_pools.default.line_template, null)
+  eem_first_vty         = try(local.device_config[each.value.name].vty_pools.eem.from_number, local.defaults.iosxr.devices.configuration.vty_pools.eem.from_number, null)
+  eem_last_vty          = try(local.device_config[each.value.name].vty_pools.eem.to_number, local.defaults.iosxr.devices.configuration.vty_pools.eem.to_number, null)
+  eem_line_template     = try(local.device_config[each.value.name].vty_pools.eem.line_template, local.defaults.iosxr.devices.configuration.vty_pools.eem.line_template, null)
+  pools = try(length(local.device_config[each.value.name].vty_pools.user_defined) == 0, true) ? null : [
+    for pool in local.device_config[each.value.name].vty_pools.user_defined : {
+      pool_name     = try(pool.name, local.defaults.iosxr.devices.configuration.vty_pools.user_defined.name, null)
+      first_vty     = try(tostring(pool.from_number), tostring(local.defaults.iosxr.devices.configuration.vty_pools.user_defined.from_number), null)
+      last_vty      = try(tostring(pool.to_number), tostring(local.defaults.iosxr.devices.configuration.vty_pools.user_defined.to_number), null)
+      line_template = try(pool.line_template, local.defaults.iosxr.devices.configuration.vty_pools.user_defined.line_template, null)
+    }
+  ]
 }
